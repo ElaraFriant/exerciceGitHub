b283b3dbc3c85016e7e7399bbb6d33a6afc56c33
ElaraFriant <121941939+ElaraFriant@users.noreply.github.com>
2023-03-21 16:06:01 +0100

    Ajout des classes Anglais Breton Version App

--- src/Kiwi.java
+++
@@ -1,8 +0,0 @@
-public class Kiwi {
-
-    private String kiwi;
-    private String wombat;
-    private String panda;
-
-
-}
--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hiiii!");
-        System.out.println("How you doin'?");
-        System.out.println("patate");
-        System.out.println("loutre");
-    }
-}
